347db4ef6653467bb89c3055eeb35ff7692c1d75
Haneen Amro <heno.amro@gmail.com>
2020-11-05 12:41:56 -0800

    connet to bpi endpoint using axios get

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,16 +2,23 @@
 
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, './client/dist')));
 
-
+//get the currency
 app.get("/currency", (req, res) => {
-
-    res.status(200).send("Helllo")
-})
+    axios.get(
+        //the end point for bpi
+        `https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-01-01&end=2020-10-10`
+    )
+        .then(response => {
+            res.status(200).send(response.data)
+        })
+        .catch(err => console.log(err));
+});
 
 
 app.listen(port, () => {
