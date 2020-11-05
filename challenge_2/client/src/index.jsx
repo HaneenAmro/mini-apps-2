80eaa337fb44dac7ee4b8d7936b08517c31acc31
Haneen Amro <heno.amro@gmail.com>
2020-11-05 12:35:21 -0800

    built up the express server

--- challenge_2/client/src/index.jsx
+++ challenge_2/client/src/index.jsx
@@ -1,2 +1,9 @@
 
 
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <App/>,
+   document.getElementById('app')
+)
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,19 @@
 
 
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+
+app.get("/currency", (req, res) => {
+
+    res.status(200).send("Helllo")
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`)
+})
